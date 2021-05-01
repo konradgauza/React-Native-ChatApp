41957566f8e3eb114c4067bd75958c07535dd791
konradgauza <konradgauza@gmail.com>
2021-05-01 17:02:12 +0200

    auth changed

--- GraphQL/Auth.ts
+++ GraphQL/Auth.ts
@@ -12,7 +12,7 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 const token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJjaGF0bHkiLCJleHAiOjE2MjE1MDY1NTYsImlhdCI6MTYxOTA4NzM1NiwiaXNzIjoiY2hhdGx5IiwianRpIjoiZTVkYTk1YjUtYzRkOC00NmMyLWI2NGEtOGYxNjMzN2UzNmJlIiwibmJmIjoxNjE5MDg3MzU1LCJzdWIiOiI4Y2IzZGRlNC1iZTRjLTRhYmQtYmI0ZS05MWE0NWI4MDk3ZGUiLCJ0eXAiOiJhY2Nlc3MifQ.YXmANaLyeh7kBM2QpA1bSIj9rpQXb86mp-g03sv7My24DvsPs4SO3E2J4spYWsU0i8McPUw9G7S0laGSAVSxjg";
 
 const httpLink = createHttpLink({
-    uri: "wss://chat.thewidlarzgroup.com/socket"
+    uri: "https://chat.thewidlarzgroup.com/api/graphql"
 });
 
 const authLink = setContext((_, { headers }) => {
